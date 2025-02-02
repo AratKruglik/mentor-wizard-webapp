fb29e9aab698cf0f656f3c0d88ad407e09fc9444
Andrey Gorbachevskiy <andrey300485@i.ua>
2025-02-02 17:08:41 +0000

    #8697mm0c7-Add-model-Currency

---
+++ database/factories/CurrencyFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\CurrencyEnum;
+use App\Models\Currency;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Currency>
+ */
+class CurrencyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->randomElement(CurrencyEnum::names()),
+            'slug' => fake()->slug(),
+            'symbol' => fake()->randomElement(CurrencyEnum::values()),
+        ];
+    }
+}
