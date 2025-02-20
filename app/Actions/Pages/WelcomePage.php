2923467ebe3f4a755c7cb556dcf81317d34f9d18
Maks Oleksyuk <maksym.oleksyuk@gmail.com>
2025-02-20 11:20:06 +0000

    #8697zpu3p: update yarn to v4

--- app/Actions/Pages/WelcomePage.php
+++ app/Actions/Pages/WelcomePage.php
@@ -1,7 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Actions\Pages;
 
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Inertia\Response;
 use Lorisleiva\Actions\Concerns\AsController;
@@ -12,6 +16,11 @@ class WelcomePage
 
     public function handle(): Response
     {
-        return Inertia::render('Welcome');
+        return Inertia::render('Welcome', [
+            'canLogin' => Route::has('login'),
+            'canRegister' => Route::has('register'),
+            'laravelVersion' => Application::VERSION,
+            'phpVersion' => PHP_VERSION,
+        ]);
     }
 }
